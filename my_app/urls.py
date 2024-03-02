b5ad6c393f15170351f020f20bdfe4f6017d218f
ambilyjerald <ambily1.mathew@gmail.com>
2024-03-02 17:03:51 +0530

    second push

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from my_app import views
 
 urlpatterns = [
-    path("",views.todo,name="test")
+    path("",views.todo,name="test"),
+    path("dashboard",views.dash,name="test1"),
+    # path("dash1",views.dash1,name='test2')
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def todo(request):
     return render(request,'index.html')
+def dash(request):
+    return render(request,'dashboard.html')
+def dash1(request):
+    return render(request,'index1.html')
+
+
